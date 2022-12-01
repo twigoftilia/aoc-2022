aa167c26353c4544c37843266c6d0bd588056c48
twigoftilia <mattias@lindqvist.nu>
2022-12-01 23:43:04 +0100

    a bit more elegant file parsing. Just because.

--- src/lib.rs
+++ src/lib.rs
@@ -19,23 +19,16 @@ pub fn rows_to_vector(input: &str) -> Vec<&str> {
 // Returns a vector of vectors of ints from input. Empty spaces gives new vector
 // i.e. 2022 day01
 pub fn vec_of_integer_vec(input: &str) -> Vec<Vec<i32>> {
-    let mut outer = vec![];
-    let mut new = true;
-    input.lines().for_each(|line| {
-        if line.is_empty() {
-            new = true;
-        } else {
-            if new {
-                outer.push(vec![]);
-                new = false;
-            }
-            outer
-                .last_mut()
-                .unwrap()
-                .push(line.trim().parse().expect("Not an integer"));
-        };
-    });
-    outer
+    let r = input
+        .split("\n\n")
+        .map(|lines| {
+            lines.lines().fold(vec![], |mut acc, line| {
+                acc.push(line.trim().parse().expect("Not an integer"));
+                acc
+            })
+        })
+        .collect();
+    r
 }
 
 fn test_inclusive_range(str: &str, min: &i32, max: &i32) -> bool {
