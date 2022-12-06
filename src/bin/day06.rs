59052cb34758178cd707e5498416e6a07ba7cd89
twigoftilia <mattias@lindqvist.nu>
2022-12-06 22:31:36 +0100

    day06 tried to elegantlify, but I think I mostly got it slower

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 static DAY_NO: &str = "06";
 static INPUT: &str = include_str!("../../inputs/day06.txt");
 // #[cfg(test)]
@@ -5,7 +7,6 @@ static INPUT: &str = include_str!("../../inputs/day06.txt");
 
 fn main() {
     let i = to_byte_array(INPUT);
-    // let d5_input = &D5Input::from_input(INPUT);
     let r1 = solve_first(i);
     println!("Day {}\n first puzzle: {}", DAY_NO, r1);
     let r2 = solve_second(&i);
@@ -22,20 +23,37 @@ fn solve_second(x1: &[u8]) -> usize {
 
 fn solve(x1: &[u8], window_length: usize) -> usize {
     //  dbg!(x1);
-    'window: for window_end in window_length..x1.len() {
-        let s = &x1[(window_end - window_length)..window_end];
 
-        for i in 0..s.len() {
-            for j in (i + 1)..(s.len()) {
-                if s[i] == s[j] {
-                    continue 'window;
-                }
+    x1.windows(window_length)
+        .map(|s| s.iter().collect::<BTreeSet<&u8>>())
+        .enumerate()
+        .find_map(|(n, s)| {
+            if s.len() == window_length {
+                return Some(n + window_length);
             }
-        }
-        return window_end;
-    }
-    panic!("PanicAngst");
+            None
+        })
+        .unwrap()
+
+    //  panic!("PanicAngst");
 }
+
+// fn solve_first_try(x1: &[u8], window_length: usize) -> usize {
+//     //  dbg!(x1);
+//     'window: for window_end in window_length..x1.len() {
+//         let s = &x1[(window_end - window_length)..window_end];
+
+//         for i in 0..s.len() {
+//             for j in (i + 1)..(s.len()) {
+//                 if s[i] == s[j] {
+//                     continue 'window;
+//                 }
+//             }
+//         }
+//         return window_end;
+//     }
+//     panic!("PanicAngst");
+// }
 
 fn to_byte_array(i: &str) -> &[u8] {
     i.lines().take(1).next().unwrap().as_bytes()
@@ -47,7 +65,6 @@ mod tests {
 
     #[test]
     fn first_example() {
-        // "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
         assert_eq!(
             solve_first(to_byte_array("mjqjpqmgbljsphdztnvjfqwrcgsmlb")),
             7
