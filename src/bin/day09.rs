536c6f57a559f5b2b38edaf0afb809f74d2fca54
twigoftilia <mattias@lindqvist.nu>
2022-12-11 00:34:00 +0100

    day9 1&2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,95 +1,106 @@
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    iter::Skip,
-};
-
-use aoc_2022::Direction;
-use nalgebra::partial_max;
+use std::collections::{BTreeMap, BTreeSet};
 
 static DAY_NO: &str = "09";
 static INPUT: &str = include_str!("../../inputs/day09.txt");
 #[cfg(test)]
 static EXAMPLE: &str = include_str!(r"../../inputs/day09_example.txt");
+#[cfg(test)]
+static EXAMPLE_2: &str = include_str!(r"../../inputs/day09_example_2.txt");
 
 fn main() {
     let input = &Instruction::list_from_input(INPUT);
-    let r1 = solve_first(&input);
+    let r1 = solve_both(&input, 2);
     println!("Day {}\n first puzzle: {}", DAY_NO, r1);
-    // let r2 = solve_second(&input);
-    // println!(" second puzzle: {}", r2);
+    let r2 = solve_both(&input, 10);
+    println!(" second puzzle: {}", r2);
 }
 
-fn solve_first(input: &Vec<Instruction>) -> usize {
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
+// fn solve_first_old(input: &Vec<Instruction>) -> usize {
+//     let mut head = (0, 0);
+//     let mut tail = (0, 0);
 
+//     let mut tail_vistits = BTreeMap::new();
+
+//     tail_vistits.insert(0, BTreeSet::from([0]));
+
+//     for i in input {
+//         //        dbg!(i);
+
+//         for _step in 0..i.steps {
+//             //  let last_tail = tail.clone();
+
+//             match i.dir {
+//                 Dir::U => head.1 -= 1,
+//                 Dir::D => head.1 += 1,
+//                 Dir::L => head.0 -= 1,
+//                 Dir::R => head.0 += 1,
+//             }
+
+//             let tail_update_option = nearest_tail_move_step(&head, &tail, &i.dir);
+
+//             match tail_update_option {
+//                 Some(pos) => {
+//                     tail = pos;
+//                 }
+//                 None => {}
+//             }
+
+//             let res = tail_vistits.get_mut(&tail.0);
+
+//             match res {
+//                 Some(v) => {
+//                     v.insert(tail.1);
+//                 }
+//                 None => {
+//                     tail_vistits.insert(tail.0, BTreeSet::from([tail.1]));
+//                 }
+//             }
+//         }
+//     }
+//     let sum = tail_vistits
+//         .values()
+//         .map(|y_in_x_pos| y_in_x_pos.iter().count())
+//         .sum();
+
+//     sum
+// }
+
+fn solve_both(input: &Vec<Instruction>, knots: usize) -> usize {
+    let mut rope: Vec<Pos> = vec![(0, 0); knots];
+    let rope_length = rope.len();
     let mut tail_vistits = BTreeMap::new();
-
     tail_vistits.insert(0, BTreeSet::from([0]));
-
     for i in input {
-        dbg!(i);
-
-        for step in 0..i.steps {
-            let last_head = head.clone();
-            let last_tail = tail.clone();
-
+        for _step in 0..i.steps {
             match i.dir {
-                Dir::U => head.1 -= 1,
-                Dir::D => head.1 += 1,
-                Dir::L => head.0 -= 1,
-                Dir::R => head.0 += 1,
+                Dir::U => rope[0].1 -= 1,
+                Dir::D => rope[0].1 += 1,
+                Dir::L => rope[0].0 -= 1,
+                Dir::R => rope[0].0 += 1,
             }
-            //            dbg!(&head);
+            for follwer_no in 1..rope_length {
+                let tail_update_option =
+                    nearest_tail_move_step(&rope[follwer_no - 1], &rope[follwer_no]);
 
-            let should_tail_move: bool =
-                ((head.0 - tail.0) as i32).abs() > 1 || ((head.1 - tail.1) as i32).abs() > 1;
-
-            if !should_tail_move {
-                continue;
-            }
-
-            // ....
-            // ....
-            // ....
-            // ....
-
-            //   0123
-            // 0 ....
-            // 1 .T.H
-            // 2 ....
-            // 3 .H.T
-
-            if head.0 - tail.0 > 1 {
-                tail.0 += 1;
-            } else if head.0 - tail.0 < -1 {
-                tail.0 -= 1;
-            }
-
-            if head.1 - tail.1 > 1 {
-                tail.1 += 1;
-            } else if head.1 - tail.1 < -1 {
-                tail.1 -= 1;
-            }
-
-            // Move in from diagonal position?
-            if last_head.0 != last_tail.0 && last_head.1 != last_tail.1 {
-                match i.dir {
-                    Dir::U | Dir::D => tail.0 = head.0,
-                    Dir::L | Dir::R => tail.1 = head.1,
+                match tail_update_option {
+                    Some(pos) => {
+                        rope[follwer_no] = pos;
+                    }
+                    None => {}
                 }
             }
 
-            dbg!(tail);
-
-            let mut res = tail_vistits.get_mut(&tail.0);
+            let res = tail_vistits.get_mut(&rope[rope_length - 1].0);
 
             match res {
                 Some(v) => {
-                    v.insert(tail.1);
+                    v.insert(rope[rope_length - 1].1);
                 }
                 None => {
-                    tail_vistits.insert(tail.0, BTreeSet::from([tail.1]));
+                    tail_vistits.insert(
+                        rope[rope_length - 1].0,
+                        BTreeSet::from([rope[rope.len() - 1].1]),
+                    );
                 }
             }
         }
@@ -102,6 +113,43 @@ fn solve_first(input: &Vec<Instruction>) -> usize {
     sum
 }
 
+fn nearest_tail_move_step(head: &Pos, tail: &Pos) -> Option<Pos> {
+    let mut new_tail = tail.clone();
+
+    //  should_tail_move at all
+    if ((head.0 - tail.0) as i32).abs() <= 1 && ((head.1 - tail.1) as i32).abs() <= 1 {
+        return None;
+    }
+    // up or down
+    if head.0 == tail.0 || head.1 == tail.1 {
+        if head.0 - tail.0 > 1 {
+            new_tail.0 += 1;
+        } else if head.0 - tail.0 < -1 {
+            new_tail.0 -= 1;
+        }
+
+        if head.1 - tail.1 > 1 {
+            new_tail.1 += 1;
+        } else if head.1 - tail.1 < -1 {
+            new_tail.1 -= 1;
+        }
+    } else {
+        if head.0 > tail.0 {
+            new_tail.0 += 1;
+        } else {
+            new_tail.0 -= 1;
+        }
+
+        if head.1 > tail.1 {
+            new_tail.1 += 1;
+        } else {
+            new_tail.1 -= 1;
+        }
+    }
+    Some(new_tail)
+}
+type Pos = (i32, i32);
+
 #[derive(Debug)]
 pub enum Dir {
     U,
@@ -111,8 +159,6 @@ pub enum Dir {
 }
 
 impl Dir {
-    pub const VALUES: [Self; 4] = [Self::U, Self::D, Self::L, Self::R];
-
     fn parse(dirname: &str) -> Dir {
         match dirname {
             "U" => Dir::U,
@@ -154,21 +200,26 @@ mod tests {
 
     #[test]
     fn first_example() {
-        assert_eq!(solve_first(&Instruction::list_from_input(EXAMPLE)), 13);
+        assert_eq!(solve_both(&Instruction::list_from_input(EXAMPLE), 2), 13);
     }
 
-    // #[test]
-    // fn first() {
-    //     assert_eq!(solve_first(&Instruction::from_input(INPUT)), 1703);
-    // }
+    #[test]
+    fn first() {
+        assert_eq!(solve_both(&Instruction::list_from_input(INPUT), 2), 6190);
+    }
 
-    // #[test]
-    // fn second_example() {
-    //     assert_eq!(solve_second(&Instruction::from_input(EXAMPLE)), 8);
-    // }
+    #[test]
+    fn second_example_1() {
+        assert_eq!(solve_both(&Instruction::list_from_input(EXAMPLE), 10), 1);
+    }
 
-    // #[test]
-    // fn second() {
-    //     assert_eq!(solve_second(&Instruction::from_input(INPUT)), 496650);
-    // }
+    #[test]
+    fn second_example_2() {
+        assert_eq!(solve_both(&Instruction::list_from_input(EXAMPLE_2), 10), 36);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(solve_both(&Instruction::list_from_input(INPUT), 10), 2516);
+    }
 }
